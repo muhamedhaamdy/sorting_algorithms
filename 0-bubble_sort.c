2173c07c1c6a8cbade89a5c445e11054de02f007
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-19 17:14:21 +0200

    "hotfix"

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -35,9 +35,8 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 				swapped = 1;
 			}
-        }
- 
-        if (!swapped)
-            break;
-    }
+		}
+	if (!swapped)
+		break;
+	}
 }
