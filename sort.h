821fb40331fc002eab9372959ec1cce9b304d078
muhamedhaamdy <mmhamdyyy@gmail.com>
2023-12-19 17:29:27 +0200

    "hotfix"

--- sort.h
+++ sort.h
@@ -21,4 +21,6 @@ typedef struct listint_s
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void swap(int *a, int *b);
+void insertion_sort_list(listint_t **list);
 #endif
